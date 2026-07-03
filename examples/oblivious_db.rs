bad10e6ec0238554ee249ac87c41e84cb7470f00
sage-p1 <github@lookingcool.org>
2026-07-03 10:59:22 -0400

    implemented oblivious_db.rs

--- examples/oblivious_db.rs
+++ examples/oblivious_db.rs
@@ -5,39 +5,60 @@
 // License, Version 2.0 found in the LICENSE-APACHE file in the root directory
 // of this source tree. You may select, at your option, one of the above-listed licenses.
 
-//! An example of using ORAM to obliviously serve an indexed database.
+//! An example of using OSAM to obliviously serve an indexed database.
 
-// extern crate oram;
+extern crate osam;
 
-// use oram::{Address, BlockSize, BlockValue, DefaultOram, Oram, OramError};
-// use rand::{rngs::OsRng, Rng};
+use osam::{BlockSize, BlockValue, Identifier, Osam, OsamError, PathOsam, TreeIndex};
+use osam::path_osam::{DEFAULT_BLOCKS_PER_BUCKET, DEFAULT_STASH_OVERFLOW_SIZE};
+use rand::{rngs::OsRng, Rng};
 
-// const BLOCK_SIZE: BlockSize = 4096;
-// const DB_SIZE: Address = 64;
-// // A stand-in for the indexed database you want to obliviously serve.
-// const DATABASE: [[u8; BLOCK_SIZE as usize]; DB_SIZE as usize] =
-//     [[0; BLOCK_SIZE as usize]; DB_SIZE as usize];
+const BLOCK_SIZE: BlockSize = 4096;
+const DB_SIZE: Identifier = 64;
+// A stand-in for the indexed database you want to obliviously serve.
+const DATABASE: [[u8; BLOCK_SIZE as usize]; DB_SIZE as usize] =
+    [[0; BLOCK_SIZE as usize]; DB_SIZE as usize];
 
-// fn main() -> Result<(), OramError> {
-//     let mut rng = OsRng;
-//     let mut oram = DefaultOram::<BlockValue<4096>>::new(DB_SIZE, &mut rng)?;
+fn main() -> Result<(), OsamError> {
+    let mut rng = OsRng;
+    let mut osam = PathOsam::<
+        BlockValue<BLOCK_SIZE>, 
+        DEFAULT_BLOCKS_PER_BUCKET,
+        >::new_with_parameters(DB_SIZE, DEFAULT_STASH_OVERFLOW_SIZE)?;
 
-//     // Read DATABASE into oram.
-//     for (i, bytes) in DATABASE.iter().enumerate() {
-//         oram.write(i as Address, BlockValue::new(*bytes), &mut rng)?;
-//     }
+    let mut addresses: [(Identifier, TreeIndex); DB_SIZE as usize] = 
+        [(Identifier::MAX, 0); DB_SIZE as usize];
 
-//     // Now oram can be used to obliviously serve the contents of DATABASE.
-//     let num_operations = 100;
-//     for _ in 0..num_operations {
-//         let random_index = rng.gen_range(0..DB_SIZE);
+    // Read DATABASE into osam.
+    for (i, bytes) in DATABASE.iter().enumerate() {
+        let address = osam.alloc(&mut rng)?;
+        addresses[i] = address;
+        let identifier = address.0;
+        let position = address.1;
+        let _ = osam.write(identifier, position, BlockValue::new(*bytes), &mut rng)?;
+    }
 
-//         let _ = oram.read(random_index, &mut rng)?;
-//     }
+    // Now osam can be used to obliviously serve the contents of DATABASE.
+    let num_operations = 100;
+    for _ in 0..num_operations {
+        // Assert addresses correctly map to DATABASE
+        let random_index = rng.gen_range(0..DB_SIZE) as usize;
+        let address = addresses[random_index];
+        let identifier = address.0;
+        let position = address.1;
+        let value = (osam.read(identifier, position)?).unwrap();
+        assert_eq!(
+            value,
+            BlockValue::new(DATABASE[random_index])
+        );
 
-//     Ok(())
-// }
+        // Write DATABASE item back
+        let address = osam.alloc(&mut rng)?;
+        addresses[random_index] = address;
+        let identifier = address.0;
+        let position = address.1;
+        let _ = osam.write(identifier, position, value, &mut rng)?;
+    }
 
-fn main() {
-    println!("Oblivious DB");
+    Ok(())
 }
